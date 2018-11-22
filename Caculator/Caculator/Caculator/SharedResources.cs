9783f4d39dc0049d32cde84fc73c068c57450b34
Rafael Soares <rafaelsoaresdes@gmail.com>
2018-11-22 16:31:11 -0200

    calc example

---
+++ Caculator/Caculator/Caculator/SharedResources.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Caculator
+{
+    public static class SharedResources
+    {
+        public static Color OpButtonBkColor
+        {
+            get { return Color.FromRgb(0xff, 0xa5, 0); }
+        }
+    }
+}
